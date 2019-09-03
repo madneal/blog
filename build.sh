67fc7db34ad792015042cad74196a8c5e47bd785
madneal <bing.ecnu@gmail.com>
2019-09-03 19:59:31 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -32,9 +32,9 @@ git commit -m "$msg"
 # Push source and build repos.
 git pull origin master
 git push origin master
-cd ../../neal1991.github.io/
-git pull
-git push all master
+# cd ../../neal1991.github.io/
+# git pull
+# git push all master
 
-# Come Back
-cd ..
+# # Come Back
+# cd ..
