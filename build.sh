f71b88d023f38de541e0a2725d89ef6d6422e7e2
madneal <bing.ecnu@gmail.com>
2025-03-14 22:24:14 +0800

    Rebuilding site Fri Mar 14 22:24:10 CST 2025

--- build.sh
+++ build.sh
@@ -1,38 +1,43 @@
-#!/bin/bash
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+#!/bin/bash  
+set -e  # Exit immediately if a command exits with a non-zero status  
 
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"  
 
-# Push Hugo content 
-git add -A
-git commit -m "$msg"
-git pull origin master
-git push origin master
+# Use current timestamp if no commit message provided  
+msg="${1:-Rebuilding site $(date)}"  
 
-#cd themes/hugo-nuo
-# git pull origin master
-#cd ../../
+# Ensure you're on the correct branch  
+git checkout master  
 
+# Pull latest changes first  
+git pull origin master  
 
-# Build the project. 
-hugo -t hugo-nuo > hugo.log
-# if using a theme, replace by `hugo -t <yourtheme>`
+# Stage all changes  
+git add -A  
 
-# Go To Public folder
-cd public/
-# Add changes to git.
-git add -A
+# Commit changes  
+git commit -m "$msg" || echo "No changes to commit"  
 
-# Commit changes.
+# Push to remote  
+git push origin master  
 
-git commit -m "$msg"
+# Build Hugo site with verbose output  
+hugo -t hugo-nuo --verbose > hugo.log  
 
-# Push source and build repos.
-git pull origin master
-git push origin master
-# git commit -m "rebuild site" --allow-empty
-# git push origin master
+# Check build log for errors  
+cat hugo.log  
 
+# Enter public directory  
+cd public  
+
+# Stage public directory changes  
+git add -A  
+
+# Commit public directory changes  
+git commit -m "$msg" || echo "No public changes to commit"  
+
+# Pull and push public directory  
+git pull origin master  
+git push origin master  
+
+echo -e "\033[0;32mDeployment completed!\033[0m"  
